21b009496ae1a4f61d49c1523f58ef92f3e57c4d
mengdage <wulalameng@gmail.com>
2014-09-13 01:55:42 +0800

    LMPC: it's just a test for commit a file

--- src/gwu/csci/arc/TestSimulator.java
+++ src/gwu/csci/arc/TestSimulator.java
@@ -4,6 +4,7 @@ public class TestSimulator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//test for github
 
 		IntegratedCircuit ic = new IntegratedCircuit();
 		CPU cpu = CPU.getInstance();
