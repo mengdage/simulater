5c55438de8b77cc0972c903a330a0a58a38c49a8
siyuanfeng <siyuanfeng@192.168.1.146>
2014-12-14 17:46:55 -0500

    New converters that convert between 18 bits binary and decimal on floating point arithmetics

--- src/gwu/csci/arc/utility/Converter.java
+++ src/gwu/csci/arc/utility/Converter.java
@@ -94,4 +94,101 @@ public class Converter {
 		}
 		return result;
 	}
+	
+	/**
+	 * 
+	 * @param s 18 bits binary floating point number
+	 * @param len length of the binary floating point number
+	 * @param dec_exp exponent of the floating point number in decimal
+	 * @return the floating point number in decimal
+	 */
+	public static double converterF2S(char[] s, int len, int dec_exp)
+	{
+		char sign;
+		char[] exp = new char[7];
+		char[] mts = new char[10];
+		
+		// floating point number in decimal
+		double dec = 0;
+		
+		// set sign
+		sign = s[0];
+		
+		// separate exponent and mantissa
+		for (int i = 1; i < 8; i++)
+		{
+			exp[i-1] = s[i];
+		}
+		for (int i = 8; i < 18; i++)
+		{
+			mts[i-8] = s[i];
+		}
+		
+		// convert exponent to decimal
+		dec_exp = conveterS2I(exp, 7);
+		
+		// convert mantissa to decimal sum
+		for (int i = 0; i < 10; i++)
+		{
+			if (mts[i] == '1')
+			{
+				dec += Math.pow(2, -(i+1));
+			}
+		}
+		
+		// combine decimal mantissa sum and exponent
+		dec = dec * Math.pow(2, dec_exp);
+		
+		// combine decimal floating point number and its sign
+		if (sign == '1')
+		{	
+			dec = dec * (-1);
+		}	
+		
+		return dec;
+	}
+	
+	/**
+	 * 
+	 * @param dec the floating point number in decimal
+	 * @param dec_exp the exponent of the floating point number in decimal
+	 * @return the floating point number in 18 bits binary
+	 */
+	public static char[] converterS2F(double dec, int dec_exp)
+	{
+		char[] s = new char[18];
+		char[] exp = new char[7];
+		
+		if (dec >= 0) s[0] = '0';
+		else
+		{
+			s[0] = '1';
+			// ignore value sign
+			dec = dec * (-1);
+		}
+		
+		// convert decimal exponent back to 7 bits binary
+		converterI2S(dec_exp, exp, 7);
+		for (int i = 0; i < 7; i++)
+		{
+			s[i+1] = exp[i];
+		}
+		
+		// divide exponent to get mantissa
+		dec = dec / Math.pow(2, dec_exp);
+		
+		// convert mantissa to sum of fractions in binary
+		for (int i = 0; i < 10; i++)
+		{
+			if (dec >= Math.pow(2, -(i+1)))
+			{
+				dec -= Math.pow(2, -(i+1));
+				s[i+8] = '1';
+			}
+			else
+				s[i+8] = '0';
+		}
+		
+		return s;
+	}
 }
